f561277d955457e53fe5739d86d861ecd3dd0c93
sonson2549 <62345196+sonson2549@users.noreply.github.com>
2020-03-29 14:48:03 +0700

    Create Exercise4_chakrit_T.py

---
+++ assignments/Exercise4_chakrit_T.py
@@ -0,0 +1,8 @@
+FE = 82.33
+GB = 79.64
+ITCS = 87.26
+CP = 89.56
+print("Foundation English: ",FE)
+print("General Business: ",GB)
+print("Introduction to Computer Systems: ",ITCS )
+print("Computer programming",CP)
