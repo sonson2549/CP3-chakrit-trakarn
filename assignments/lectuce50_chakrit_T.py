aca944a39ffb06576a5dfdb8191cfa5667266eab
sonson2549 <62345196+sonson2549@users.noreply.github.com>
2020-04-04 14:39:48 +0700

    Create lectuce50_chakrit_T.py

---
+++ assignments/lectuce50_chakrit_T.py
@@ -0,0 +1,13 @@
+def addNumber (x, y):
+    print(x+y)
+def minusNumber(a,b):
+    print(a-b)
+def multiNumber(c,d):
+    print(c*d)
+def devideNumber(e,f):
+    print(e/f)
+addNumber(5,5)
+minusNumber(5,5)
+multiNumber(5,5)
+devideNumber(5,5)
+
